c363cd1fb381af57ed809364edda4098a8736d00
Nantanat-Poyomratanasin <nantanat.poyomratanasin@gmail.com>
2026-05-15 15:32:01 +0700

    mongoDB V2

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -4,3 +4,7 @@ import { router as userRouter } from "./users.routes.js";
 export const router = Router();
 
 router.use("/users", userRouter);
+
+//router.use("/products", productRouter);
+
+//router.use("/notes", notesRouter);
---
+++ src/routes/v2/users.routes.js
@@ -0,0 +1,80 @@
+import { Router } from "express";
+import { User } from "../../modules/users/user.model.js";
+
+export const router = Router();
+
+const userResponse = (doc) => {
+  const user = doc.toObject();
+  //only delete in Server not DB
+  delete user.password;
+  return user;
+};
+
+router.get("/", async (req, res) => {
+  try {
+    const users = await User.find();
+    return res.status(200).json({ success: true, data: users });
+  } catch (error) {
+    return res.status(400).json({ success: false, error: error });
+  }
+});
+
+router.post("/", async (req, res) => {
+  const { username, email, password, role } = req.body || {};
+
+  if (!username || !email || !password) {
+    const err = new Error("Username, email, and password are required");
+    err.name = "ValidationError";
+    err.status = 400;
+    return res.status(400).json({ success: false, error: err });
+  }
+
+  try {
+    const doc = await User.create({ username, email, password, role });
+    return res.status(201).json({ success: true, data: userResponse(doc) });
+  } catch (err) {
+    return res.status(400).json({ success: false, error: err });
+  }
+});
+
+router.put("/:id", async (req, res) => {
+  const user = users.find((u) => u.id === req.params.id);
+
+  if (!user) {
+    return res.status(404).json({ error: "User not found" });
+  }
+
+  const { username, email, password } = req.body;
+
+  if (!username || !email || !password) {
+    return res
+      .status(400)
+      .json({ error: "Username, email, and password are required" });
+  }
+
+  user.username = username;
+  user.email = email;
+  user.password = password;
+  res.status(200).json(user);
+});
+
+router.delete("/:id", async (req, res) => {
+  const user = users.find((u) => u.id === req.params.id);
+
+  if (!user) {
+    return res.status(404).json({ error: "User not found" });
+  }
+
+  const { username, email, password } = req.body;
+
+  if (!username || !email || !password) {
+    return res
+      .status(400)
+      .json({ error: "Username, email, and password are required" });
+  }
+
+  user.username = username;
+  user.email = email;
+  user.password = password;
+  res.status(200).json(user);
+});
