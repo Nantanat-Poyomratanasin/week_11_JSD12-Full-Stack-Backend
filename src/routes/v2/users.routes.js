5f2c4fbe25448b7b063e181dec4176622777af76
Nantanat-Poyomratanasin <nantanat.poyomratanasin@gmail.com>
2026-05-21 10:52:08 +0700

    jwt

--- src/routes/v2/users.routes.js
+++ src/routes/v2/users.routes.js
@@ -1,4 +1,7 @@
 import { Router } from "express";
+import { bcrypt } from "bcrypt";
+import jwt from "jsonwebtoken";
+
 import { User } from "../../modules/users/user.model.js";
 import { supabase } from "../../config/supabase.js";
 import {
@@ -100,7 +103,68 @@ router.get("/", getUsers);
 
 router.post("/", createUserHash);
 
-router.post("/login", usersLogin);
+// router.post("/login", usersLogin);
+
+//############################## JWT ###########################################################
+router.post("/login", async (req, res, next) => {
+  //เอา email and password ออกจาก req.body
+  const { email, password } = req.body;
+
+  //checkว่าข้อมูลที่ได้รับมาถูกต้องมั้ย
+  if (!emaill || !password) {
+    //ส่งข้อมูลกลับไป
+    return res
+      .status(400)
+      .json({ success: false, message: "Email and Password required!" });
+  }
+
+  try {
+    const user = await User.findOne({ email }).select("+password");
+
+    if (!user) {
+      return res
+        .status(400)
+        .json({ success: false, message: "User not found!" });
+    }
+    const isMatched = await bcrypt.compare(password, user.password);
+    if (!isMatched) {
+      return res
+        .status(400)
+        .json({ success: false, message: "incorrect password!" });
+    }
+    //ok -->  แจก token
+    //ใส่ 3  parameter คือ user id, secret key ใน .env,เวลาหมดอายุของtoken
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1h",
+    }); //1 hrs expiration
+
+    const isProd = process.env.NODE_ENV === "production";
+
+    //เก็บ token ใน cookie
+    //ใส่ 3 paremeter --> ชื่อที่ๆเราจะไปเก็บใน cookie, ตัวแปร token, config
+    res.cookie("accessToken", token, {
+      httpOnly: true,
+      secure: isProd, //only send over HTTPS in production
+      sameSite: isProd ? "none" : "lax",
+      path: "/",
+      maxAge: 60 * 60 * 1000, // 1hr
+    });
+
+    return res.status(200).json({
+      success: true,
+      message: "login successful",
+      user: {
+        _id: user._id,
+        username: user.username,
+        email: user.email,
+        role: user.role,
+      },
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+//############################## JWT ###########################################################
 
 router.put("/:id", updateUser);
 
