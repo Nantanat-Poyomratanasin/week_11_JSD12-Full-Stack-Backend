b8c5916713d2518a8bd2fc32a08b0894d0df5eb2
Nantanat-Poyomratanasin <nantanat.poyomratanasin@gmail.com>
2026-05-27 18:04:17 +0700

    test round 3

--- src/routes/v2/index.js
+++ src/routes/v2/index.js
@@ -4,6 +4,7 @@ import { router as userRouter } from "./users.routes.js";
 export const router = Router();
 
 router.use("/users", userRouter);
+router.use("/users/supabase", usersSupabaseRoutes);
 
 //router.use("/products", productRouter);
 
